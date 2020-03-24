cc20f4a78bbf185f89ceb8ea3f0a4332c5bdd674
erwan933 <62592118+erwan933@users.noreply.github.com>
2020-03-24 17:06:55 +0100

    av6

--- index.php
+++ index.php
@@ -7,7 +7,25 @@
     <?php include'header.php'?>
     <?php include'landing/banner.php'?>
     
-    
+    <div class="container">
+       
+       <section class="section1">
+        <h2>Choisissez votre idéal de vacances<br> avec LocaSun</h2>
+        
+        <div class="row">
+          <div class="col-md-3"><img src="https://via.placeholder.com/250x130" alt=""><center>Vacances à la mer</center></div>
+            <div class="col-md-3"><img src="https://via.placeholder.com/250x130" alt=""><center>Vacances à la montagne</center></div>
+            <div class="col-md-3"><img src="https://via.placeholder.com/250x130" alt=""><center>Vacances à la campagne</center></div>
+            <div class="col-md-3"><img src="https://via.placeholder.com/250x130" alt=""><center>Archipels</center></div>
+        </div>
+        
+        
+        
+        
+        
+        </section>
+        
+    </div>
     
     
     
