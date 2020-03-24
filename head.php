3625cebe27ec7ad3c361bd8a988d755904a44570
erwan933 <62592118+erwan933@users.noreply.github.com>
2020-03-24 14:53:17 +0100

    corps

---
+++ head.php
@@ -0,0 +1,5 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Locasun | Votre agence de voyage</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include'head.php'?>
+<title>Locasun | Votre agence de voyage</title>
+<body>
+    <?php include'header.php'?>
+</body>
+</html>
