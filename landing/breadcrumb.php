2edf9833e0b42e985ab604ec014c77feac031d8e
erwan933 <62592118+erwan933@users.noreply.github.com>
2020-03-31 16:51:23 +0200

    breadcrumb

--- landing/breadcrumb.php
+++ landing/breadcrumb.php
@@ -2,9 +2,14 @@
     <div class="container " >
          <div class="row">
              <div class="col-md-2"></div>
-             <!--<div class="col-md-8 breadcrumb-bar">
-                 Accueil / <?php echo basename($_SERVER['PHP_SELF']); ?>
-             </div>-->
+             <div class="col-md-8 breadcrumb-bar" style="text-transform: capitalize;">
+                 Accueil / <?php echo substr($_SERVER['PHP_SELF'],0,-4);  ?>
+                 
+                 
+        
+                 
+                 
+             </div>
               <div class="col-md-2"></div>
          </div>
         
